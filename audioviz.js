a773be734fd6ab05be53dd376139c8065b4c26f3
Walter Murphy <walter.murphy@gmail.com>
2013-08-08 21:42:05 -0700

    Make client/server work with node-static+http+socket.io. Very painful.

--- audioviz.js
+++ audioviz.js
@@ -1,22 +1,24 @@
-var alsa = require('alsa');
-var fft = require('fft');
-var sockio = require('socket.io');
 
-// deafult for socket IO.
+var alsa = require('alsa');
+var FFT = require('fft');
+var sockio = require('socket.io');
+var http = require('http');
+var static = require('node-static');
+
+// default socket port.
 var port = 8080;
 
 // defaults for audio capture.
 var devin = 'plughw:0,0';                // ALSA device
+var devin_gain = 30;                     // good default for my mic.
 
 var channels = 2,                        // Stereo
     rate = 44100,                        // Sample rate
     format = alsa.FORMAT_S16_LE,         // PCM format (signed 16bit LE int)
     access = alsa.ACCESS_RW_INTERLEAVED, // Access mode
     latency = 300;                       // Desired latency in millisecs
-var stream_on = false;    // flag for getting the next audio chunk.
 
-var io = sockio.listen(8080);
-io.set('log level', 0);
+var stream_on = false;    // flag for getting the next audio chunk.
   
 // The Capture class is a stream.Readable subclass.
 var capture = new alsa.Capture(devin,channels,rate,format,access,latency);
@@ -28,11 +30,11 @@ if ( capture === null ) {
 var nachunk = 1024;  // size of an audio chunk.
 
 // make an FFT instance
-var ffti = new fft.complex(nachunk, false);
+var fft = new FFT.complex(nachunk, false);
 
-var iachunk = new Float64Array(2 * nachunk);
-var oachunk = new Float64Array(2 * nachunk);
-var HanWin  = new Float64Array(nachunk);
+var iachunk = new Float32Array(2 * nachunk);
+var oachunk = new Float32Array(2 * nachunk);
+var HanWin  = new Float32Array(nachunk);
 var SpectraSlice = new Float32Array(nachunk/2);
 var twopi = 2 * Math.PI;
 
@@ -41,64 +43,103 @@ for ( i=0; i<nachunk; i++) {
    HanWin[i] = 0.5 * (1-Math.cos(twopi * i /(nachunk-1)));
 }
 
+console.log(__dirname);
 
-// Now the event handlers:
+  var fsrv = new static.Server('./');
 
+  function fileHandler(req, res) {
+    req.addListener('end', function () {
+      console.log("Request URL: " + req.url);
+      fsrv.serve(req, res);
+//  });
+    }).resume();
+  }
+
+
+
+  var httpServer = http.createServer(fileHandler);
+
+  var io = sockio.listen(httpServer);
+  io.set('log level', 0);
+
+  httpServer.listen(port);
+
+
+
+  // Now the event handlers:
   io.sockets.on('connection', function (socket) {
-  //  socket.emit('news', { hello: 'world' });
+    // Send vertical step size in Hz.
+    socket.emit('freqstep', rate/nachunk/2);
 
+    // Control Events Start and Stop data collection, processing and transfer.
     socket.on('control event', function (data) {
       console.log(data);
       if ( data['cmd'] == 'start' ) {
         stream_on = true;
         capture.resume();
-        console.log('Resume');
       }
       else if ( data['cmd'] == 'stop') {
         stream_on = false;
         capture.pause();
-        console.log('Pause');
       }
     });
 
-  capture.on('readable', function () {
-    var buf = capture.read(nachunk * 4); // 4 bytes per frame
-    if ( buf === null ) {
-      console.error("No audio captured: " + devin);
-      process.exit();
-    }
-    //console.log("Got a chunk");
+    capture.on('readable', function () {
+      var buf = capture.read(nachunk * 4); // 4 bytes per frame
+      if ( buf === null ) {
+        console.error("No audio captured: " + devin);
+        process.exit();
+      }
 
-    for ( i=0; i< nachunk; i++ ) {
-      iachunk[i] = // average L & R channels together
-        (buf.readInt16LE(i*4) + buf.readInt16LE((i*4)+2))/2;
-    }
+      // Combine L & R channels together and amplify the volume.
+      for ( i=0; i< nachunk; i++ ) {
+        iachunk[i] =
+          (buf.readInt16LE(i*4) + buf.readInt16LE((i*4)+2))
+          * devin_gain;
+      }
 
-  //TODO  Add a low-pass filter here.
+      // Apply Han Window to audio input section
+      // for ( i=0; i<nachunk; i++) {
+      //    iachunk *= HanWin[i];
+      // }
 
-    // Apply Han Window to audio input section
-  //  for ( i=0; i<nachunk; i++) {
-  //     iachunk *= HanWin[i];
-  //  }
+      var re, im, max_eng;
+      // Need to pick max value to normalize energy level during its display.
+      // Energy levels in a spectral slice can range from 0-255,
+      // normalize to that; this is a display limitation. Can add 
+      // AGC to this later.
+      max_eng = 1000000;
 
-    var re, im;
-    var fstep = rate / nachunk / 2;
-    ffti.simple(oachunk, iachunk, 'complex'); // 'complex' or 'real'
-    for ( i=0; i< nachunk/2; i++ ) {
-      re = oachunk[2*i];
-      im = oachunk[2*i+1];
-      SpectraSlice[i] =  Math.sqrt((re*re)+(im*im));
-    }
-    socket.emit('freqstep', fstep );
-    socket.emit('slice', SpectraSlice );
-//    console.log("Sent Spectrogram Slice");
-    if ( stream_on === true ) {
-      capture.read(0);
-    }
-  });
+      fft.simple(oachunk, iachunk, 'complex'); // 'complex' or 'real'
+      var tmp;
+      for ( i=0; i< nachunk/2; i++ ) {
+        re = oachunk[2*i];
+        im = oachunk[2*i+1];
+        tmp = ( 255 * Math.sqrt((re*re)+(im*im)) ) / max_eng;
+        if ( tmp > 255 ) { tmp = 255; }
+        SpectraSlice[i] = Math.floor(tmp);
+      }
+
+      // Scale Y-axis by dropping top half and expanding lower half.
+      var explower = []; 
+      for ( i=0; i< nachunk; i++ ) {
+        explower[i] = SpectraSlice[i];
+      }
+      for ( i=0; i< nachunk; i+=2 ) {
+        SpectraSlice[i]   = explower[i/2];
+        SpectraSlice[i+1] = explower[i/2];
+      }
+
+      // Send Spectral energy array for entire slice.
+      socket.emit('slice', SpectraSlice );
+
+      // Cause next slice data capture to be scheduled.
+      if ( stream_on === true ) {
+        capture.read(0);
+      }
+    });
 
   });
 
 //--------------------------------------------
-
 
